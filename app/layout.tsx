6ba12e2874518bdfc99672463c5f57c3e5ad3cae
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-11-06 20:33:46 +0700

    feat: supress hydration

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      suppressHydrationWarning
+      suppressContentEditableWarning
+      lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
