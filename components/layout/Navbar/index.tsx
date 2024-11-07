d1c1175c58328d233aca6cf830c3183afcdfb156
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-11-07 12:07:35 +0700

    feat: initialize dummy navbar

--- components/layout/Navbar/index.tsx
+++ components/layout/Navbar/index.tsx
@@ -1,7 +1,16 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export const Navbar = () => {
   return (
-    <nav></nav>
+    <nav className='fixed top-0 w-full bg-white shadow-md px-32 py-5 flex justify-between items-center'>
+      <div className='space-y-2'>
+        <h1 className='font-bold text-3xl'>SIJARTA</h1>
+        <p>by NgeQuery</p>
+      </div>
+      <div>
+        <Button>Login</Button>
+      </div>
+    </nav>
   )
 }
