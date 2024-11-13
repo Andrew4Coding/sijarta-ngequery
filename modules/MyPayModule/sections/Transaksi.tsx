edfca2a77b2c3f6cd342340def04cabfe2c42dec
Tristan A. <tristanagraa@gmail.com>
2024-11-13 07:07:23 +0700

    feat: mypay transaction

---
+++ modules/MyPayModule/sections/Transaksi.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React, { useState } from "react";
+import { KategoriCombobox } from "../elements/KategoriCombobox";
+import { TopUp } from "../elements/TopUp";
+import { Withdrawal } from "../elements/Withdrawal";
+import { Transfer } from "../elements/Transfer";
+import { BayarTransaksi } from "../elements/BayarTransaksi";
+
+export const Transaksi = ({ saldo }: { saldo: string }) => {
+  const [value, setValue] = useState("");
+  return (
+    <div className="flex flex-col">
+      <p className="font-semibold">
+        Saldo kamu: <span className="text-green-700">{saldo}</span>
+      </p>
+      <KategoriCombobox value={value} setValue={setValue} />
+      {value === "TopUp MyPay" && <TopUp />}
+      {value === "Withdrawal" && <Withdrawal />}
+      {value === "Transfer MyPay" && <Transfer />}
+      {value === "Membayar Transaksi" && <BayarTransaksi />}
+      {value === "" && <p className="text-center font-medium text-gray-400 text-lg">Tidak ada kategori yang dipilih.</p>}
+    </div>
+  );
+};
