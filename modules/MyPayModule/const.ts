dcd6438eef3fa2f53ea4a42fdc23b5c5ebd35027
Tristan A. <tristanagraa@gmail.com>
2024-11-13 07:06:29 +0700

    feat: mypay const

--- modules/MyPayModule/const.ts
+++ modules/MyPayModule/const.ts
@@ -1,4 +1,71 @@
-import { paymentHistoryInterface, PaymentType } from "./interface";
+import {
+  paymentHistoryInterface,
+  PaymentType,
+  BayarTransaksiInterface,
+} from "./interface";
+
+export const dummyBayarTransaksiData: BayarTransaksiInterface[] = [
+  {
+    id: "1",
+    amount: 73659,
+    date: new Date(2024, 10, 6, 14, 23, 45),
+    kategori: "Cleaning Service",
+  },
+  {
+    id: "2",
+    amount: 67757,
+    date: new Date(2024, 10, 6, 14, 23, 45),
+    kategori: "Cleaning Service",
+  },
+  {
+    id: "3",
+    amount: 39186,
+    date: new Date(2024, 10, 6, 14, 23, 45),
+    kategori: "Antar Makanan",
+  },
+  {
+    id: "4",
+    amount: 86642000,
+    date: new Date(2024, 10, 6, 14, 23, 45),
+    kategori: "Antar Makanan",
+  },
+  {
+    id: "5",
+    amount: 88046,
+    date: new Date(2024, 10, 6, 14, 23, 45),
+    kategori: "Antar Makanan",
+  },
+  {
+    id: "6",
+    amount: 12034,
+    date: new Date(2024, 10, 6, 14, 23, 45),
+    kategori: "Car Service",
+  },
+  {
+    id: "7",
+    amount: 41863,
+    date: new Date(2025, 1, 2, 9, 12, 30),
+    kategori: "Car Service",
+  },
+  {
+    id: "8",
+    amount: 73160,
+    date: new Date(2025, 1, 2, 9, 12, 30),
+    kategori: "Car Service",
+  },
+  {
+    id: "9",
+    amount: 97712,
+    date: new Date(2025, 1, 2, 9, 12, 30),
+    kategori: "Car Service",
+  },
+  {
+    id: "10",
+    amount: 84407,
+    date: new Date(2025, 1, 2, 9, 12, 30),
+    kategori: "Home Service",
+  },
+];
 
 export const dummyData: paymentHistoryInterface[] = [
   {
@@ -181,4 +248,11 @@ export const dummyData: paymentHistoryInterface[] = [
     date: new Date(2025, 2, 5, 16, 55, 40),
     type: "Membayar Transaksi",
   },
+];
+
+export const paymentTypeData: PaymentType[] = [
+  "Withdrawal",
+  "TopUp MyPay",
+  "Transfer MyPay",
+  "Membayar Transaksi",
 ];
--- modules/MyPayModule/interface.ts
+++ modules/MyPayModule/interface.ts
@@ -1,8 +1,19 @@
-export type PaymentType = "Withdrawal" | "TopUp MyPay" | "Transfer MyPay" | "Membayar Transaksi";
+export type PaymentType =
+  | "Withdrawal"
+  | "TopUp MyPay"
+  | "Transfer MyPay"
+  | "Membayar Transaksi";
 
 export interface paymentHistoryInterface {
   id: string;
   amount: number;
   date: Date;
   type: PaymentType;
+}
+
+export interface BayarTransaksiInterface {
+  id: string;
+  amount: number;
+  date: Date;
+  kategori: string;
 }
