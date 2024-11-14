6fa5dbec61c31f1482c82333fcdee9afd1184eec
Tristan A. <tristanagraa@gmail.com>
2024-11-14 13:45:07 +0700

    fix: responsiveness

--- modules/MyPayModule/index.tsx
+++ modules/MyPayModule/index.tsx
@@ -9,6 +9,7 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Transaksi } from "./sections/Transaksi";
+import { TransaksiHistori } from "./sections/TransaksiHistori";
 
 export const MyPayModule = () => {
   const saldo = dummyData
@@ -36,65 +37,7 @@ export const MyPayModule = () => {
             <Transaksi saldo={saldo} />
           </DialogContent>
         </Dialog>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-4 md:gap-y-6 h-[700px] overflow-y-scroll overflow-x-visible w-full">
-          {dummyData.map((item, index) => {
-            const date = new Date(item.date.toISOString()).toLocaleDateString(
-              "id-ID",
-              {
-                day: "2-digit",
-                month: "2-digit",
-                year: "numeric",
-              }
-            );
-            const hours = new Date(item.date.toISOString()).toLocaleTimeString(
-              "en-US",
-              {
-                hour: "2-digit",
-                minute: "2-digit",
-              }
-            );
-            return (
-              <React.Fragment key={item.id}>
-                {index === 0 ||
-                (index > 0 &&
-                  dummyData[index - 1].date.getDate() !==
-                    item.date.getDate()) ? (
-                  <div className="md:col-span-2 md:text-center text-gray-700 text-base md:text-xl font-bold mt-4">
-                    {date}
-                  </div>
-                ) : null}
-                <div
-                  className={cn(
-                    "rounded-lg bg-red-50 px-2 md:px-6 py-4 shadow-lg flex justify-between items-center hover:scale-105 transition-transform md:mx-6",
-                    { "bg-blue-50": item.type === "TopUp MyPay" }
-                  )}
-                >
-                  <div className="flex flex-col gap-2">
-                    <p
-                      className={cn(
-                        "text-[14px] md:text-2xl font-semibold text-red-500",
-                        {
-                          "text-green-500": item.type === "TopUp MyPay",
-                        }
-                      )}
-                    >
-                      {item.amount.toLocaleString("id-ID", {
-                        style: "currency",
-                        currency: "IDR",
-                      })}
-                    </p>
-                    <p className="text-[12px] md:text-base text-gray-500 font-semibold">
-                      {hours}
-                    </p>
-                  </div>
-                  <p className="text-[12px] md:text-xl font-bold text-[#1f1e1eb7]">
-                    {item.type}
-                  </p>
-                </div>
-              </React.Fragment>
-            );
-          })}
-        </div>
+        <TransaksiHistori />
       </div>
     </main>
   );
