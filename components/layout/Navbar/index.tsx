007be87e5117cd93fe24047ada5e6e8b3b65498e
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-11-16 14:02:52 +0700

    fix: diskon module styling

--- components/layout/Navbar/index.tsx
+++ components/layout/Navbar/index.tsx
@@ -1,15 +1,40 @@
 import { Button } from '@/components/ui/button'
+import { LogOut } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 export const Navbar = () => {
+  const {role, isAuthenticated} = {role: 'pengguna', isAuthenticated: true}
+  
   return (
     <nav className='fixed top-0 w-full bg-white shadow-md px-32 py-5 flex justify-between items-center z-50 font-dmsans'>
       <div className='space-y-2'>
         <h1 className='font-extrabold text-3xl font-dmsans'>SIJARTA</h1>
-        <p className='text-sm'>by NgeQuery</p>
+        <p className='text-sm'>
+          Welcome {' '}
+          <strong>
+            {role === 'pengguna' ? 'Pengguna' : 'Admin'}
+            !
+          </strong>
+        </p>
+      </div>
+      <div className='flex gap-4'>
+        <Link href='/'>Home</Link>
+        <Link href='/mypay'>MyPay</Link>
+        <Link href='/'>Pesanan Saya</Link>
+        <Link href='/diskon'>Diskon</Link>
+        <Link href='/profile'>Profile</Link>
+        <Link href='/login'>Logout</Link>
       </div>
       <div>
-        <Button>Login</Button>
+        {isAuthenticated ? 
+          <Button className='gap-2'>
+            <LogOut />  
+            Logout
+          </Button> 
+          : 
+          <Button>Login</Button>
+        }
       </div>
     </nav>
   )
--- modules/DiskonModule/index.tsx
+++ modules/DiskonModule/index.tsx
@@ -1,43 +1,22 @@
 "use client";
 
-import React, { useState } from 'react';
-import ModalPembelian from '@/modules/DiskonModule/elements/ModalPembelian';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Separator } from '@/components/ui/separator';
 
 export interface Voucher {
   code: string;
   discount: string;
-  minTransaction: string;
+  minTransaction: number;
   validity: string;
   quota: string;
-  price: string;
+  price: number;
 }
 
 export const DiskonModule = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isSuccess, setIsSuccess] = useState(true);
-  const [selectedVoucher, setSelectedVoucher] = useState<Voucher | null>(null);
-
-  const handleVoucherPurchase = (voucher: Voucher) => {
-    const userBalance = 100000;
-    const voucherPrice = parseInt(voucher.price.replace("Rp ", "").replace(".", ""));
-
-    if (userBalance >= voucherPrice) {
-      setIsSuccess(true);
-    } else {
-      setIsSuccess(false);
-    }
-    setSelectedVoucher(voucher);
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
   const voucherData: Voucher[] = [
-    { code: "DISKON50", discount: "50%", minTransaction: "Rp 100.000", validity: "30 Hari", quota: "10x", price: "Rp 50.000" },
-    { code: "DISKON30", discount: "30%", minTransaction: "Rp 75.000", validity: "15 Hari", quota: "5x", price: "Rp 30.000" },
-    { code: "DISKON20", discount: "20%", minTransaction: "Rp 50.000", validity: "7 Hari", quota: "2x", price: "Rp 20.000" },
+    { code: "DISKON50", discount: "50%", minTransaction: 100000, validity: "30 Hari", quota: "10x", price: 50000 },
+    { code: "DISKON30", discount: "30%", minTransaction: 75000, validity: "15 Hari", quota: "5x", price: 30000 },
+    { code: "DISKON20", discount: "20%", minTransaction: 50000, validity: "7 Hari", quota: "2x", price: 20000 },
   ];
 
   const promoData = [
@@ -47,173 +26,84 @@ export const DiskonModule = () => {
   ];
 
   return (
-    <div style={styles.pageContainer}>
-      <h1 style={styles.title}>DISKON</h1>
+    <div className="px-10 md:px-20 font-dmsans py-32">
+      <h1 className="text-center font-bold text-2xl my-10">DISKON</h1>
 
       {/* Voucher Section */}
-      <section style={styles.section}>
-        <h2 style={styles.sectionTitle}>Voucher</h2>
-        <div style={styles.voucherContainer}>
+      <section className="mb-12">
+        <h2 className="font-bold mb-2 text-2xl">Voucher</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 flex-wrap gap-5 justify-between">
           {voucherData.map((voucher, index) => (
-            <div key={index} style={styles.voucherBox}>
-              <h3 style={styles.voucherTitle}>{voucher.code}</h3>
-              <div style={styles.voucherDetail}>
-                <span style={styles.voucherLabel}>Potongan</span>
-                <span style={styles.voucherValue}>{voucher.discount}</span>
+            <div key={index} className="w-full border border-gray-300 p-5 rounded-2xl bg-white shadow-md">
+              <h3 className="font-bold text-xl text-center mb-4">{voucher.code}</h3>
+              <div className="flex justify-between items-center mb-2">
+                <span className="text-gray-800 text-sm">Potongan</span>
+                <span className="text-blue-600 font-bold text-xs md:text-sm">{voucher.discount}</span>
               </div>
-              <hr style={styles.divider} />
-              <div style={styles.voucherDetail}>
-                <span style={styles.voucherLabel}>Min Transaksi Pemesanan</span>
-                <span style={styles.voucherValue}>{voucher.minTransaction}</span>
+              <Separator className='my-2 border-[0.5px] border-black/5' />
+              <div className="flex justify-between items-center mb-2">
+                <span className="text-gray-800 text-xs md:text-sm">Min Transaksi Pemesanan</span>
+                <span className="text-blue-600 font-bold text-xs md:text-sm">Rp {voucher.minTransaction}</span>
               </div>
-              <hr style={styles.divider} />
-              <div style={styles.voucherDetail}>
-                <span style={styles.voucherLabel}>Jumlah Hari Berlaku</span>
-                <span style={styles.voucherValue}>{voucher.validity}</span>
+              <Separator className='my-2 border-[0.5px] border-black/5' />
+              <div className="flex justify-between items-center mb-2">
+                <span className="text-gray-800 text-xs md:text-sm">Jumlah Hari Berlaku</span>
+                <span className="text-blue-600 font-bold text-xs md:text-sm">{voucher.validity}</span>
               </div>
-              <hr style={styles.divider} />
-              <div style={styles.voucherDetail}>
-                <span style={styles.voucherLabel}>Kuota Penggunaan</span>
-                <span style={styles.voucherValue}>{voucher.quota}</span>
+              <Separator className='my-2 border-[0.5px] border-black/5' />
+              <div className="flex justify-between items-center mb-2">
+                <span className="text-gray-800 text-xs md:text-sm">Kuota Penggunaan</span>
+                <span className="text-blue-600 font-bold text-xs md:text-sm">{voucher.quota}</span>
               </div>
-              <hr style={styles.divider} />
-              <div style={styles.voucherDetail}>
-                <span style={styles.voucherLabel}>Harga</span>
-                <span style={styles.voucherValue}>{voucher.price}</span>
+              <Separator className='my-2 border-[0.5px] border-black/5' />
+              <div className="flex justify-between items-center mb-2">
+                <span className="text-gray-800 text-xs md:text-sm">Harga</span>
+                <span className="text-blue-600 font-bold text-xs md:text-sm">Rp {voucher.price}</span>
               </div>
-              <button style={styles.buyButton} onClick={() => handleVoucherPurchase(voucher)}>Beli Voucher</button>
+              <Dialog>
+                <DialogTrigger className="w-full bg-blue-500 text-white py-2 rounded-lg text-sm hover:bg-blue-600 duration-300">
+                  Beli Voucher
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle className="text-center">{voucher.price >= 10000 ? 'Sukses' : 'Gagal' }</DialogTitle>
+                  </DialogHeader>
+                  <div className="flex flex-col gap-2">
+                    <p className="mb-4">
+                      {(
+                        voucher.price >= 10000
+                      ) ? (
+                        <>
+                          Selamat! Anda berhasil membeli voucher dengan kode <strong>{voucher.code}</strong>. Voucher ini akan berlaku hingga tanggal <strong>{voucher.validity}</strong> dengan kuota penggunaan sebanyak <strong>{voucher.quota}</strong>.
+                        </>
+                      ) : (
+                        <>
+                          Maaf, saldo Anda tidak cukup untuk membeli voucher ini. Silakan top-up saldo Anda.
+                        </>
+                      )}
+                    </p>
+                  </div>
+                </DialogContent>
+              </Dialog>
             </div>
           ))}
         </div>
       </section>
 
       {/* Promo Section */}
-      <section style={styles.section}>
-        <h2 style={styles.sectionTitle}>Promo</h2>
-        <div style={styles.promoContainer}>
+      <section className="mb-12">
+        <h2 className="font-bold mb-2 text-2xl">Promo</h2>
+        <div className="flex flex-col gap-5">
           {promoData.map((promo, index) => (
-            <div key={index} style={styles.promoBox}>
-              <div style={styles.promoRow}>
-                <span style={styles.promoCode}>{promo.code}</span>
-                <span style={styles.promoEndDate}>{promo.endDate}</span>
-              </div>
+            <div key={index} className="border border-gray-300 p-5 rounded-2xl bg-white shadow-md flex justify-between items-center">
+              <span className="font-bold text-lg text-gray-800">{promo.code}</span>
+              <span className="text-gray-500 text-sm">{promo.endDate}</span>
             </div>
           ))}
         </div>
       </section>
-
-      {/* Use ModalPembelian component */}
-      <ModalPembelian
-        isModalOpen={isModalOpen}
-        isSuccess={isSuccess}
-        selectedVoucher={selectedVoucher}
-        closeModal={closeModal}
-      />
     </div>
   );
 };
-
-const styles = {
-  pageContainer: {
-    padding: '20px',
-    marginLeft: '80px',
-    marginRight: '80px',
-  },
-  title: {
-    textAlign: 'center' as 'center',
-    fontWeight: 'bold',
-    fontSize: '24px',
-    marginTop: '40px',
-    marginBottom: '20px',
-  },
-  section: {
-    marginBottom: '50px',
-  },
-  sectionTitle: {
-    fontWeight: 'bold',
-    marginBottom: '10px',
-  },
-  voucherContainer: {
-    display: 'flex',
-    flexWrap: 'wrap' as 'wrap',
-    gap: '20px',
-    justifyContent: 'space-between',
-  },
-  voucherBox: {
-    width: '30%',
-    border: '1px solid #d9d9d9',
-    padding: '20px',
-    borderRadius: '20px',
-    backgroundColor: '#ffffff',
-    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-  },
-  voucherTitle: {
-    fontWeight: 'bold',
-    fontSize: '20px',
-    textAlign: 'center' as 'center',
-    marginBottom: '15px',
-  },
-  voucherDetail: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: '10px',
-  },
-  voucherLabel: {
-    color: '#333',
-    fontSize: '16px',
-  },
-  voucherValue: {
-    color: '#007BFF',
-    fontSize: '16px',
-    fontWeight: 'bold',
-  },
-  divider: {
-    border: 'none',
-    borderTop: '1px solid #d9d9d9',
-    margin: '10px 0',
-  },
-  buyButton: {
-    marginTop: '15px',
-    width: '100%',
-    padding: '10px',
-    border: 'none',
-    borderRadius: '12px',
-    backgroundColor: '#007BFF',
-    color: '#fff',
-    fontWeight: 'bold',
-    cursor: 'pointer',
-  },
-  promoContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '20px',
-  },
-  promoBox: {
-    border: '1px solid #d9d9d9',
-    padding: '20px',
-    borderRadius: '20px',
-    backgroundColor: '#ffffff',
-    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  promoRow: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '100%',
-  },
-  promoCode: {
-    fontWeight: 'bold',
-    color: '#333',
-    fontSize: '18px',
-  },
-  promoEndDate: {
-    color: '#777',
-    fontSize: '14px',
-  },
-} as const;
 
 export default DiskonModule;
