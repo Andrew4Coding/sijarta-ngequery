4de6ac20029f5d6226c7a0091e164c3448e2c752
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-11-17 22:51:03 +0700

    feat: profile pengguna

--- modules/ProfilePageModule/edit/index.tsx
+++ modules/ProfilePageModule/edit/index.tsx
@@ -6,7 +6,7 @@ import { z } from 'zod';
 import { Button } from "@/components/ui/button";
 import { ArrowLeft, Save } from "lucide-react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import {
     FormControl,
     FormField,
@@ -20,13 +20,31 @@ const ProfileSchema = z.object({
     Nama: z.string().nonempty("Nama is required"),
     Level: z.string().nonempty("Level is required"),
     JenisKelamin: z.string().nonempty("Jenis Kelamin is required"),
-    NoHP: z.string().nonempty("No HP is required"),
-    TanggalLahir: z.string().nonempty("Tanggal Lahir is required"),
+    NoHP: z.string().nonempty("No HP is required").regex(/^\d+$/, "No HP must be a number"),
+    TanggalLahir: z.string().nonempty("Tanggal Lahir is required").regex(/^\d{2} \w+ \d{4}$/, "Tanggal Lahir must be in the format DD MMMM YYYY"),
     Alamat: z.string().nonempty("Alamat is required"),
-    SaldoMPAY: z.string().nonempty("Saldo MPAY is required")
+    SaldoMPAY: z.string().nonempty("Saldo MPAY is required").regex(/^Rp \d+(\.\d{3})*$/, "Saldo MPAY must be in the format Rp X.XXX.XXX")
 });
 
-export const EditProfilePageModule = () => {
+const ProfilePekerjaSchema = z.object({
+    Nama: z.string().nonempty("Nama is required"),
+    JenisKelamin: z.string().nonempty("Jenis Kelamin is required"),
+    NoHP: z.string().nonempty("No HP is required").regex(/^\d+$/, "No HP must be a number"),
+    TanggalLahir: z.string().nonempty("Tanggal Lahir is required").regex(/^\d{2} \w+ \d{4}$/, "Tanggal Lahir must be in the format DD MMMM YYYY"),
+    Alamat: z.string().nonempty("Alamat is required"),
+    SaldoMPAY: z.string().nonempty("Saldo MPAY is required").regex(/^Rp \d+(\.\d{3})*$/, "Saldo MPAY must be in the format Rp X.XXX.XXX"),
+    NamaBank: z.string().nonempty("Nama Bank is required"),
+    NoRekening: z.string().nonempty("No Rekening is required").regex(/^\d+$/, "No Rekening must be a number"),
+    NPWP: z.string().nonempty("NPWP is required").regex(/^\d+$/, "NPWP must be a number"),
+    Rating: z.string().nonempty("Rating is required").regex(/^[1-5]$/, "Rating must be between 1 and 5"),
+    JumlahPesananSelesai: z.string().nonempty("Jumlah Pesanan Selesai is required").regex(/^\d+$/, "Jumlah Pesanan Selesai must be a number"),
+    KategoriJasa: z.array(z.string()).nonempty("Kategori Jasa is required")
+});
+
+export const EditProfilePengguna = () => {
+    const searchParams = useSearchParams();
+    const roleParams = searchParams.get('role') ?? 'pengguna';
+
     const form = useForm<z.infer<typeof ProfileSchema>>({
         resolver: zodResolver(ProfileSchema),
         defaultValues: {
@@ -44,7 +62,7 @@ export const EditProfilePageModule = () => {
 
     function onSubmit(data: z.infer<typeof ProfileSchema>) {
         console.log(data);
-        router.replace("/profile");
+        router.replace(`/profile?role${roleParams}`);
     }
 
     return (
@@ -94,5 +112,95 @@ export const EditProfilePageModule = () => {
                 </form>
             </Form>
         </main>
-    );
+    )    
+}
+
+export const EditProfilePekerja = () => { 
+    const searchParams = useSearchParams();
+    const roleParams = searchParams.get('role') ?? 'pengguna';
+    
+    const form = useForm<z.infer<typeof ProfilePekerjaSchema>>({
+        resolver: zodResolver(ProfilePekerjaSchema),
+        defaultValues: {
+            Nama: "Andrew Devito Aryo",
+            JenisKelamin: "Laki-laki",
+            NoHP: "08123456789",
+            TanggalLahir: "01 Januari 1990",
+            Alamat: "Jl. Kebon Jeruk No. 123, Jakarta",
+            SaldoMPAY: "Rp 1.000.000",
+            NamaBank: "BCA",
+            NoRekening: "1234567890",
+            NPWP: "1234567890",
+            Rating: "5",
+            JumlahPesananSelesai: "100",
+            KategoriJasa: ["Cleaning Service", "Antar Makanan", "Car Service"]
+        }
+    });
+
+    const router = useRouter();
+
+    function onSubmit(data: z.infer<typeof ProfilePekerjaSchema>) {
+        console.log(data);
+        router.replace(`/profile?role=${roleParams}`);
+    }
+
+    return (
+        <main className="flex flex-col my-40 px-10 md:px-32 font-dmsans gap-5">
+            <div className="flex gap-4 items-center">
+                <ArrowLeft className="w-4 cursor-pointer hover:scale-105" onClick={() => {
+                    router.back();
+                }} />
+                <h1 className="font-bold text-xl">Edit Profile Page</h1>
+            </div>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+                    <Image
+                        src="/psql.png"
+                        alt="Profile Picture"
+                        width={150}
+                        height={150}
+                        className="rounded-full"
+                    />
+                    <div className="grid grid-cols-2 gap-5">
+                        {Object.keys(form.getValues()).map((key) => (
+                            <FormField
+                                key={key}
+                                control={form.control}
+                                name={key as keyof z.infer<typeof ProfilePekerjaSchema>}
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>{key}</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                label=''
+                                                className="border p-2"
+                                                type="text"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        ))}
+                    </div>
+                    <Button type="submit">
+                        <Save className="w-4" />
+                        Save Changes
+                    </Button>
+                </form>
+            </Form>
+        </main>
+    )
+}
+
+export const EditProfilePageModule = () => {
+    const searchParams = useSearchParams();
+    const roleParams = searchParams.get('role') ?? 'pengguna';
+
+    return (
+        <main>
+            {roleParams === 'pengguna' ? <EditProfilePengguna /> : <EditProfilePekerja />}
+        </main>
+    )
 };
--- modules/ProfilePageModule/index.tsx
+++ modules/ProfilePageModule/index.tsx
@@ -2,8 +2,12 @@
 import { Button } from "@/components/ui/button";
 import { Pencil } from "lucide-react";
 import Image from "next/image";
+import { useRouter, useSearchParams } from "next/navigation";
 
-export const ProfilePageModule = () => {
+export const ProfilePengguna = () => {
+    const searchParams = useSearchParams();
+    const roleParams = searchParams.get('role') ?? 'pengguna';
+
     return (
         <main className="my-40 px-10 md:px-32 font-dmsans flex flex-col gap-5">
             <Image
@@ -44,7 +48,7 @@ export const ProfilePageModule = () => {
                     <p className="text-sm">Rp 1.000.000</p>
                 </div>
                 <div className="flex flex-col gap-2">
-                    <a href="/profile/edit">
+                    <a href={`/profile/edit?role=${roleParams}`}>
                         <Button>
                             <Pencil className="w-4" />
                             Edit Profile
@@ -53,5 +57,100 @@ export const ProfilePageModule = () => {
                 </div>
             </div>
         </main>
+    )
+}
+
+export const ProfilePekerja = () => {
+    const router = useRouter();
+    const kategoriJasa = [
+        "Cleaning Service",
+        "Antar Makanan",
+        "Car Service"
+    ]
+
+    const searchParams = useSearchParams();
+    const roleParams = searchParams.get('role') ?? 'pengguna';
+
+    return (
+        <main className="my-40 px-10 md:px-32 font-dmsans flex flex-col gap-5">
+            <Image
+                src="/psql.png"
+                alt="Profile Picture"
+                width={150}
+                height={150}
+                className="rounded-full"
+            />
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                <div className="flex flex-col gap-2">
+                    <h3 className="font-bold text-2xl">Nama</h3>
+                    <p className="text-sm">Andrew Devito Aryo</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h3 className="font-bold text-2xl">Jenis Kelamin</h3>
+                    <p className="text-sm">Laki-laki</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h3 className="font-bold text-2xl">No HP</h3>
+                    <p className="text-sm">08123456789</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h3 className="font-bold text-2xl">Tanggal Lahir</h3>
+                    <p className="text-sm">01 Januari 1990</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h3 className="font-bold text-2xl">Alamat</h3>
+                    <p className="text-sm">Jl. Kebon Jeruk No. 123, Jakarta</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h3 className="font-bold text-2xl">Saldo MPAY</h3>
+                    <p className="text-sm">Rp 1.000.000</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h3 className="font-bold text-2xl">Nama Bank</h3>
+                    <p className="text-sm">Bank Mandiri</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h3 className="font-bold text-2xl">No Rekening</h3>
+                    <p className="text-sm">1234567890</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h3 className="font-bold text-2xl">NPWP</h3>
+                    <p className="text-sm">01.234.567.8-901.000</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h3 className="font-bold text-2xl">Rating</h3>
+                    <p className="text-sm">4.8</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h3 className="font-bold text-2xl">Jumlah Pesanan Selesai</h3>
+                    <p className="text-sm">125</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h3 className="font-bold text-2xl">Kategori Jasa</h3>
+                    {
+                        kategoriJasa.map((kategori, index) => (
+                            <p key={index} className="text-sm">{kategori}</p>
+                        ))
+                    }
+                </div>
+                <Button className="col-span-2"
+                    onClick={() => {
+                        router.push(`/profile/edit?role=${roleParams}`);
+                    }}
+                >
+                    <Pencil className="w-4" />
+                    Edit Profile
+                </Button>
+            </div>
+        </main>
     );
+};
+
+
+export const ProfilePageModule = () => {
+    const searchParams = useSearchParams();
+    const roleParams = searchParams.get('role') ?? 'pengguna';
+
+    return (roleParams === 'pengguna' ? <ProfilePengguna /> : <ProfilePekerja />)
 }
