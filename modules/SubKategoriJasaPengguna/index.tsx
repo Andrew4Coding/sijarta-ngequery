183e1d974bf123e5a5f2f2a47a60f59c999e6a18
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-11-18 10:49:48 +0700

    feat: merge

--- modules/SubKategoriJasaPengguna/index.tsx
+++ modules/SubKategoriJasaPengguna/index.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { subcategoryData } from './const';
+import { subcategoryData } from '../SubKategoriJasa/const';
 
 // Fungsi untuk membersihkan format harga
 const cleanPrice = (price: string): number => {
