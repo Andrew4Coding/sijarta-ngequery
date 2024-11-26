2366f86e1f788e929ad899714fef2c7e5fa62a42
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-11-26 21:54:45 +0700

    chore: change db url

--- database/db.ts
+++ database/db.ts
@@ -1,11 +1,16 @@
 import { Pool } from 'pg';
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+// });
+
+// Pool with db Url
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-});
+  connectionString: process.env.DATABASE_URL,
+})
 
 export default pool;
