2aac7a83564d71dd99e0e36cae255fd454797f06
Tristan A. <tristanagraa@gmail.com>
2024-11-26 12:30:41 +0700

    feat: tailwind config colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,17 @@ const config: Config = {
 		  },
   		colors: {
   			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
+  			foreground: 'var(--foreground)',
+			"green-50": "#E8F7EF",
+			"green-100": "#B8E7CD",
+			"green-200": "#96DCB5",
+			"green-300": "#66CC94",
+			"green-400": "#48C27F",
+			"green-500": "#2FAA6A",
+			"green-600": "#18A356",
+			"green-700": "#127F43",
+			"green-800": "#0E6234",
+			"green-900": "#0B4B28",
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
