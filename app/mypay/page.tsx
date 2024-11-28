17b0a3099315a039a477f32e3b06eead46d46ad6
Tristan A. <tristanagraa@gmail.com>
2024-11-29 00:14:36 +0700

    feat: take cookies in server

--- app/mypay/page.tsx
+++ app/mypay/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { MyPayModule } from "@/modules/MyPayModule";
+import { cookies } from "next/headers";
+import { decode } from "jsonwebtoken";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+  const token = await cookies().then((cookie) => cookie.get("sessionToken")?.value);
+  !token && redirect("/login");
+
+  const decoded = decode(token!);
+  console.log(decoded)
+
+
   return <MyPayModule />;
 };
 
