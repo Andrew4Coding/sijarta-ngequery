2f2033e1c952c13a7e8b563e94a6c1e9aeceea1c
Tristan A. <tristanagraa@gmail.com>
2024-11-29 15:37:56 +0700

    feat: sonner toaster

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
+import { Toaster } from "sonner";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
       <Navbar />
+      <Toaster position="top-center" />
       <main className="bg-green-50">{children}</main>
       <Footer />
     </main>
