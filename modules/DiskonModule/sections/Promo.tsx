f882911bafc5100a57e64c2132122efee45e79ff
aileenjeha <aileenjeha@gmail.com>
2024-12-01 01:56:03 +0700

    feat: promo responsive

--- modules/DiskonModule/sections/Promo.tsx
+++ modules/DiskonModule/sections/Promo.tsx
@@ -2,24 +2,24 @@
 
 import React from "react";
 import { Promo } from "../interface";
-import { dateConverter } from "../../EditProfileModule";  
+import { dateConverter } from "../../EditProfileModule";
 
 export const PromoSection = ({ promoData }: { promoData: Promo[] }) => {
   return (
-    <div className="w-full max-w-[1280px] p-12 bg-white rounded-[20px] border border-[#d9d9d9] flex flex-col justify-start items-start gap-10">
+    <div className="w-full max-w-[1280px] p-4 md:p-12 bg-white rounded-[20px] border border-[#d9d9d9] flex flex-col justify-start items-start gap-10">
       <div className="self-stretch p-2 bg-[#1ab35f] rounded-xl pl-5">
         <div className="text-white text-[32px] font-bold">Promo</div>
       </div>
       <div className="self-stretch flex flex-col justify-start items-start gap-5">
         {promoData.length > 0 ? (
-          // Wrapper for two promos per row
-          <div className="grid grid-cols-2 gap-5 w-full">
+          // Wrapper for two promos per row on larger screens and one on smaller screens
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 w-full">
             {promoData.map((promo, index) => (
               <div key={index} className="w-full flex items-start gap-5">
-                <div className="w-full h-[57px] p-5 bg-white rounded-xl border border-[#d9d9d9] flex justify-between items-center gap-3">
-                  <div className="text-black text-2xl font-bold">{promo.kode}</div>
-                  <div className="text-[#1ab35f] text-xl font-normal">
-                    {dateConverter(promo.tglakhirberlaku)} 
+                <div className="w-full h-[57px] p-4 sm:p-5 bg-white rounded-xl border border-[#d9d9d9] flex justify-between items-center gap-3">
+                  <div className="text-black text-lg sm:text-2xl font-bold">{promo.kode}</div>
+                  <div className="text-[#1ab35f] text-sm sm:text-xl font-normal">
+                    {dateConverter(promo.tglakhirberlaku)}
                   </div>
                 </div>
               </div>
@@ -27,8 +27,8 @@ export const PromoSection = ({ promoData }: { promoData: Promo[] }) => {
           </div>
         ) : (
           <div className="self-stretch flex items-start gap-5">
-            <div className="grow h-[57px] p-5 bg-white rounded-xl border border-[#d9d9d9] flex justify-center items-center">
-              <div className="text-gray-500">No promo available</div>
+            <div className="grow h-[57px] p-4 sm:p-5 bg-white rounded-xl border border-[#d9d9d9] flex justify-center items-center">
+              <div className="text-gray-500 text-sm sm:text-base">No promo available</div>
             </div>
           </div>
         )}
