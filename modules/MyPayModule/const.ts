4173f0f668c624925ccefb424560dde8eab3263b
Tristan A. <tristanagraa@gmail.com>
2024-11-30 15:06:08 +0700

    feat: bank const

--- modules/MyPayModule/const.ts
+++ modules/MyPayModule/const.ts
@@ -1,69 +1,26 @@
 import {
   PaymentType,
-  BayarTransaksiInterface,
 } from "./interface";
 
-export const dummyBayarTransaksiData: BayarTransaksiInterface[] = [
-  {
-    id: "1",
-    amount: 73659,
-    date: new Date(2024, 10, 6, 14, 23, 45),
-    kategori: "Cleaning Service",
-  },
-  {
-    id: "2",
-    amount: 67757,
-    date: new Date(2024, 10, 6, 14, 23, 45),
-    kategori: "Cleaning Service",
-  },
-  {
-    id: "3",
-    amount: 39186,
-    date: new Date(2024, 10, 6, 14, 23, 45),
-    kategori: "Antar Makanan",
-  },
-  {
-    id: "4",
-    amount: 86642000,
-    date: new Date(2024, 10, 6, 14, 23, 45),
-    kategori: "Antar Makanan",
-  },
-  {
-    id: "5",
-    amount: 88046,
-    date: new Date(2024, 10, 6, 14, 23, 45),
-    kategori: "Antar Makanan",
-  },
-  {
-    id: "6",
-    amount: 12034,
-    date: new Date(2024, 10, 6, 14, 23, 45),
-    kategori: "Car Service",
-  },
-  {
-    id: "7",
-    amount: 41863,
-    date: new Date(2025, 1, 2, 9, 12, 30),
-    kategori: "Car Service",
-  },
-  {
-    id: "8",
-    amount: 73160,
-    date: new Date(2025, 1, 2, 9, 12, 30),
-    kategori: "Car Service",
-  },
-  {
-    id: "9",
-    amount: 97712,
-    date: new Date(2025, 1, 2, 9, 12, 30),
-    kategori: "Car Service",
-  },
-  {
-    id: "10",
-    amount: 84407,
-    date: new Date(2025, 1, 2, 9, 12, 30),
-    kategori: "Home Service",
-  },
+export const listOfBank = [
+  "Bank BCA",
+  "Bank Mandiri",
+  "Bank BNI",
+  "Bank BRI",
+  "Bank CIMB Niaga",
+  "Bank Danamon",
+  "Bank Permata",
+  "Bank BTPN",
+  "Bank OCBC NISP",
+  "Bank Panin",
+  "Bank Maybank",
+  "Bank Mega",
+  "Bank UOB Indonesia",
+  "Bank DBS Indonesia",
+  "Bank HSBC Indonesia",
+  "Bank BTN",
+  "Bank BJB",
+  "Bank DKI",
 ];
 
 export const paymentTypeData: PaymentType[] = [
