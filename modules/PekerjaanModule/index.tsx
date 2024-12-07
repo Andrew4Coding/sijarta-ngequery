18408b4420fa7447002dcc465420db41b76da227
Tristan A. <tristanagraa@gmail.com>
2024-12-08 01:11:20 +0700

    feat: pekerjaan page

--- modules/PekerjaanModule/index.tsx
+++ modules/PekerjaanModule/index.tsx
@@ -1,8 +1,16 @@
-"use client";
 import { Pekerjaan } from "./sections/Pekerjaan";
 import Image from "next/image";
+import { cookies } from "next/headers";
+import { decode, JwtPayload } from "jsonwebtoken";
+import { redirect } from "next/navigation";
 
-export const PekerjaanModule = () => {
+export const PekerjaanModule = async () => {
+  const token = await cookies().then(
+    (cookie) => cookie.get("sessionToken")?.value
+  );
+  !token && redirect("/login");
+
+  const decoded = decode(token!);
   return (
     <main className="min-h-screen flex flex-col justify-center px-6 md:px-20 py-32 z-10">
       <div className="absolute top-0 w-full left-0 h-full z-[1]">
@@ -11,7 +19,7 @@ export const PekerjaanModule = () => {
       <h1 className="header text-[60px] mt-[70px] font-newake text-green-500 text-center z-10">
         Pekerjaan
       </h1>
-      <Pekerjaan />
+      <Pekerjaan userId={(decoded as JwtPayload).data.id} />
     </main>
   );
 };
--- modules/PekerjaanModule/sections/Pekerjaan.tsx
+++ modules/PekerjaanModule/sections/Pekerjaan.tsx
@@ -1,45 +1,81 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
-import { pekerjaanCards, pekerjaanCategories } from "../const";
 import { KategoriCombobox } from "../elements/KategoriCombobox";
 import { SubKategoriCombobox } from "../elements/SubKategoriCombobox";
-import { PekerjaanCardProps } from "../interface";
+import { PekerjaanCardProps, subKategoriInterface } from "../interface";
 
-export const Pekerjaan = () => {
+export const Pekerjaan = ({ userId }: { userId: string }) => {
   const [value, setValue] = useState("");
   const [subValue, setSubValue] = useState("");
   const [filteredPekerjaan, setfilteredPekerjaan] = useState<
     PekerjaanCardProps[]
   >([]);
+  const [subCategories, setSubCategories] = useState<subKategoriInterface[]>(
+    []
+  );
+
+  const fetchAllPekerjaan = async () => {
+    const response = await fetch(`/api/pekerjaan?id=${userId}`);
+    if (response.ok) {
+      const responseData = await response.json();
+      const data: PekerjaanCardProps[] = responseData.data.availableJobs;
+      setfilteredPekerjaan(data);
+      setSubCategories(responseData.data.subKategoriPekerja);
+      console.log(responseData);
+    } else {
+      const error = await response.json();
+      console.log(error);
+    }
+  };
+
+  const fetchPekerjaanBySubCategory = async (subCategory: string) => {
+    const response = await fetch(
+      `/api/pekerjaan?id=${userId}&subCategory=${subCategory}`
+    );
+    if (response.ok) {
+      const responseData = await response.json();
+      const data: PekerjaanCardProps[] = responseData.data.availableJobs;
+      setfilteredPekerjaan(data);
+    } else {
+      const error = await response.json();
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     const filterData = async () => {
-      if (value === ""){
+      if (value === "") {
         setSubValue("");
       }
 
       if (value === "" && subValue === "") {
-        setfilteredPekerjaan(pekerjaanCards);
+        fetchAllPekerjaan();
         return;
       }
-      const filteredData = pekerjaanCards.filter(
-        (card) => card.subCategory === subValue && value !== ""
-      );
-      setfilteredPekerjaan(filteredData);
+      fetchPekerjaanBySubCategory(subValue);
     };
     filterData();
   }, [value, subValue]);
+
+  useEffect(() => {
+    fetchAllPekerjaan();
+  }, []);
+
   return (
     <section className="flex flex-col gap-12 z-10">
       <div className="mx-auto md:space-x-4 max-md:flex max-md:flex-col max-md:gap-1">
-        <KategoriCombobox value={value} setValue={setValue} />
+        <KategoriCombobox
+          value={value}
+          setValue={setValue}
+          categories={subCategories.map((category) => category.namaKategori)}
+        />
         <SubKategoriCombobox
           value={subValue}
           setValue={setSubValue}
           subCategory={
-            pekerjaanCategories.find((card) => card.category === value)
-              ?.subCategories || []
+            subCategories.find((status) => status.namaKategori === value)
+              ?.subKategori || []
           }
         />
       </div>
@@ -50,7 +86,10 @@ export const Pekerjaan = () => {
           </div>
         ) : (
           filteredPekerjaan.map((card) => (
-            <div key={card.id} className="flex lg:flex-row flex-col lg:gap-5 items-center">
+            <div
+              key={card.id}
+              className="flex lg:flex-row flex-col lg:gap-5 items-center"
+            >
               <div className="flex flex-col font-bold w-full lg:w-[30%] xl:w-1/5">
                 <div className="bg-green-500 rounded-t-[12px] py-5 md:py-6 text-[20px] md:text-[24px] text-white text-center">
                   {card.subCategory}
@@ -65,13 +104,11 @@ export const Pekerjaan = () => {
               </div>
               <div className="w-full h-full flex md:flex-row flex-col max-md:gap-4 justify-between items-center bg-white rounded-b-[20px] lg:rounded-[20px] max-lg:border-t-0 border border-[#D9D9D9] p-6 md:p-9">
                 <div className="md:space-y-4 font-bold max-md:text-center">
-                  <h2 className="text-[24px] md:text-[28px]">{card.assignner}</h2>
+                  <h2 className="text-[24px] md:text-[28px]">
+                    {card.assignner}
+                  </h2>
                   <p className="text-[16px] md:text-[20px] text-[#B2B2B2]">
-                    {new Date(card.todoDate).toLocaleDateString("id-ID", {
-                      day: "2-digit",
-                      month: "2-digit",
-                      year: "numeric",
-                    })}
+                    {card.sesi} Sesi
                   </p>
                 </div>
                 <h2 className="text-lg text-[24px] md:text-[28px] font-bold text-green-500">
@@ -80,7 +117,12 @@ export const Pekerjaan = () => {
                     currency: "IDR",
                   })}
                 </h2>
-                <Button variant={'secondary'} className="md:text-[20px] max-md:w-full">Kerjakan Pesanan</Button>
+                <Button
+                  variant={"secondary"}
+                  className="md:text-[20px] max-md:w-full"
+                >
+                  Kerjakan Pesanan
+                </Button>
               </div>
             </div>
           ))
