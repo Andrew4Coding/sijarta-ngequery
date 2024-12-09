ada181b00b36f286846bcf4438c133201f5c3be7
Tristan A. <tristanagraa@gmail.com>
2024-12-09 19:44:54 +0700

    feat: batalkan pesanan

--- app/api/orders/status/route.ts
+++ app/api/orders/status/route.ts
@@ -1,35 +1,34 @@
 import pool from "@/database/db";
 import { StatusPesanan } from "@/database/models/statusPesanan";
+import { TrPemesananStatus } from "@/database/models/trPemesananStatus";
 
 export async function POST(req: Request) {
-    try {
-        const { orderId } = await req.json();
+  try {
+    const { orderId } = await req.json();
 
-        if (!orderId) {
-            return new Response(
-                JSON.stringify({ message: "Order ID is required" }),
-                { status: 400 }
-            );
-        }
-
-        const cancelStatusId = await new StatusPesanan().findBy("nama", "Pesanan Dibatalkan");
-
-        const query = `
-            INSERT INTO tr_pemesanan_status (idtrpemesanan, idstatus, tglwaktu)
-            VALUES ($1, $2, NOW())
-        `;
-
-        await pool.query(query, [orderId, cancelStatusId?.id]);
-
-        return new Response(
-            JSON.stringify({ message: "Order canceled successfully" }),
-            { status: 200 }
-        );
-    } catch (error) {
-        console.error("Error canceling order:", error);
-        return new Response(
-            JSON.stringify({ message: "Failed to cancel order" }),
-            { status: 500 }
-        );
+    if (!orderId) {
+      return new Response(JSON.stringify({ message: "Order ID is required" }), {
+        status: 400,
+      });
     }
+
+    const cancelStatusId = await new StatusPesanan().findBy(
+      "nama",
+      "Pesanan Dibatalkan"
+    );
+
+    await new TrPemesananStatus().update("idtrpemesanan", orderId, {
+      idstatus: cancelStatusId?.id,
+    });
+
+    return new Response(
+      JSON.stringify({ message: "Order canceled successfully" }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error canceling order:", error);
+    return new Response(JSON.stringify({ message: "Failed to cancel order" }), {
+      status: 500,
+    });
+  }
 }
