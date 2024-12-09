1edf026a37eb44aee5589d5d94c35afb6e2c4c38
Tristan A. <tristanagraa@gmail.com>
2024-12-09 19:33:43 +0700

    feat: join kategori jasa

--- app/api/subkategori/pekerja/route.ts
+++ app/api/subkategori/pekerja/route.ts
@@ -1,14 +1,53 @@
-export async function POST(req: Request) { 
-    try {
-        const query = new URL(req.url).searchParams;
-        const pekerjaId = query.get("pekerjaId");
-        const kategoriJasaId = query.get("kategoriJasaId");
+import { KategoriJasa } from "@/database/models/kategoriJasa";
+import { PekerjaKategoriJasa } from "@/database/models/pekerjaKategoriJasa";
+
+export async function POST(req: Request) {
+  try {
+    const query = new URL(req.url).searchParams;
+    const pekerjaId = query.get("pekerjaId");
+    const kategoriJasa = query.get("kategoriJasa");
+
+    if (!pekerjaId || !kategoriJasa) {
+      return new Response(
+        JSON.stringify({
+          message: "Pekerja ID and Kategori Jasa ID are required",
+        }),
+        { status: 400 }
+      );
     }
 
-    catch (error) {
-        return new Response(
-            JSON.stringify({ message: "Internal Server Error", error: error instanceof Error ? error.message : "Unknown error" }),
-            { status: 500, headers: { "Content-Type": "application/json" } }
-        );
+    const kategoriJasaModel = await new KategoriJasa().findBy(
+      "namakategori",
+      kategoriJasa
+    );
+
+    if (!kategoriJasaModel) {
+      return new Response(
+        JSON.stringify({
+          message: "Kategori Jasa not found",
+        }),
+        { status: 404 }
+      );
     }
+
+    await new PekerjaKategoriJasa().create({
+      pekerjaid: pekerjaId,
+      kategorijasaid: kategoriJasaModel.id,
+    });
+
+    return new Response(
+      JSON.stringify({
+        message: "Pekerja successfully joined the category",
+      }),
+      { status: 200 }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        message: "Internal Server Error",
+        error: error instanceof Error ? error.message : "Unknown error",
+      }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
+  }
 }
