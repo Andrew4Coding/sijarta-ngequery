76e46c8560f708c15b5d0887801ea5c6eeac5481
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-12-09 09:21:59 +0700

    fix: user registration trigger updated

--- database/seed.ts
+++ database/seed.ts
@@ -506,6 +506,9 @@ export async function seedTrigger() {
         RETURNS TRIGGER AS $$
         BEGIN
             IF EXISTS (SELECT 1 FROM PEKERJA WHERE npwp = NEW.npwp AND id != NEW.id) THEN
+                IF TG_OP = 'INSERT' THEN
+                    DELETE FROM USERTABLE WHERE id = NEW.id;
+                END IF; -- Fixed here
                 RAISE EXCEPTION 'NPWP already registered';
             END IF;
             RETURN NEW;
@@ -526,6 +529,9 @@ export async function seedTrigger() {
                 AND nomorrekening = NEW.nomorrekening
                 AND id != NEW.id
             ) THEN
+                IF TG_OP = 'INSERT' THEN
+                    DELETE FROM USERTABLE WHERE id = NEW.id;
+                END IF; -- Fixed here
                 RAISE EXCEPTION 'Bank name and account number combination already registered for another worker';
             END IF;
             RETURN NEW;
@@ -535,6 +541,7 @@ export async function seedTrigger() {
         CREATE TRIGGER trigger_check_bank_account
         BEFORE INSERT OR UPDATE ON PEKERJA
         FOR EACH ROW EXECUTE FUNCTION check_bank_account_combination();
+
     `);
 
     console.log('Seeding trigger done!');
