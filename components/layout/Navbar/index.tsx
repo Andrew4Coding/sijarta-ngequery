de9ad72c9468ef0cc087f9de29113430ddd45f9b
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-12-09 10:47:45 +0700

    fix: currency in navbar

--- components/layout/Navbar/index.tsx
+++ components/layout/Navbar/index.tsx
@@ -1,8 +1,9 @@
 'use client'
 import { Button } from '@/components/ui/button'
-import { ChevronDown, LogOut } from 'lucide-react'
+import { ChevronDown } from 'lucide-react'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import {
@@ -42,7 +43,6 @@ export const Navbar = () => {
 
   const hideMenus: boolean = pathname === '/login' || pathname === '/register'
 
-
   // Fetch User Saldo
   const [userDataState, setUserDataState] = useState<UserType>({} as UserType);
   async function fetchUserProfile() {
@@ -72,7 +72,6 @@ export const Navbar = () => {
 
       setIsOpen(false)
 
-      
       setTimeout(() => {
         router.push('/login')
       }
@@ -81,74 +80,85 @@ export const Navbar = () => {
   }
 
   return (
-    <nav className={`fixed top-0 w-full bg-white shadow-sm px-10 lg:px-32 py-8 flex justify-between items-center z-50`}>
+    <motion.nav
+      initial={{ y: -120 }}
+      animate={{ y: 0 }}
+      transition={{ duration: 0.5, ease: 'easeInOut' }}
+      className={`fixed top-0 w-full bg-white shadow-sm px-10 lg:px-32 py-8 flex justify-between items-center z-50`}
+    >
       <h1 className='font-extrabold text-4xl font-newake text-green-500'>SIJARTA</h1>
       <div className='hidden lg:flex gap-12'>
-        {isAuthenticated && (role === 'pelanggan' ? pelangganMenus : pekerjaMenus).map(menu => (
-          <Link key={menu.href} href={menu.href} className='hover:font-extrabold duration-300'>{menu.label}</Link>
-        ))}
+      {isAuthenticated && (role === 'pelanggan' ? pelangganMenus : pekerjaMenus).map(menu => (
+        <Link key={menu.href} href={menu.href} className='hover:font-extrabold duration-300'>{menu.label}</Link>
+      ))}
       </div>
       <div className='flex gap-4'>
-        <div className='hidden lg:flex'>
-          {isAuthenticated ?
-            <div className='flex gap-2 items-center'>
-              <div className='flex items-center gap-2 text-sm'>
-                <div className='text-right'>
-                  <p className='font-bold text-base'>Hello, {userData.nama}</p>
-                  <p className='font-medium text-sm'>Rp {userDataState.saldompay ?? "Loading ..."}</p>
-                </div>
-                <Avatar
-                  className='cursor-pointer'
-                  onClick={() => {
-                    router.push('/profile')
-                  }}
-                >
-                  <AvatarImage src={userData.linkfoto ?? `/images/profpic_placeholder.png`} />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-              </div>
-            </div>
-            :
-            <Button
-              className={`${hideMenus ? 'hidden' : ''}`}
-              onClick={() => router.push('/login')}
-            >Login</Button>
-          }
+      <div className='hidden lg:flex'>
+        {isAuthenticated ?
+        <div className='flex gap-2 items-center'>
+          <div className='flex items-center gap-2 text-sm'>
+          <div className='text-right'>
+            <p className='font-bold text-base'>Hello, {userData.nama}</p>
+            <p className='font-medium text-sm'>
+            {
+            Number(userDataState.saldompay).toLocaleString("id-ID", {
+              style: "currency",
+              currency: "IDR",
+            })
+            ?? "Loading ..."}</p>
+          </div>
+          <Avatar
+            className='cursor-pointer'
+            onClick={() => {
+            router.push('/profile')
+            }}
+          >
+            <AvatarImage src={userData.linkfoto ?? `/images/profpic_placeholder.png`} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          </div>
         </div>
-        <Popover
-          onOpenChange={(open) => setIsOpen(open)}
-        >
-          <PopoverTrigger className=''>
-            <ChevronDown
-              className={`w-6 h-6 ${isOpen ? 'rotate-180' : ''} duration-200 ease-in-out ${hideMenus ? 'hidden' : ''}`}
-            />
-          </PopoverTrigger>
-          <PopoverContent className='bg-white w-fit rounded-xl'>
-            <ul className='space-y-2 '>
-              <div className='space-y-2 lg:hidden'>
-                {(role === 'pelanggan' ? pelangganMenus : pekerjaMenus).map(menu => (
-                  <li key={menu.href}>
-                    <Link href={menu.href}>{menu.label}</Link>
-                  </li>
-                ))}
-              </div>
-              <li>
-                <Link href='/profile'>Profile</Link>
-              </li>
-              <Separator />
-              <li>
-                <a
-                  href='/'
-                  onClick={logout}
-                  className='text-red-500 cursor-pointer duration-300'
-                >
-                  Logout
-                </a>
-              </li>
-            </ul>
-          </PopoverContent>
-        </Popover>
+        :
+        <Button
+          className={`${hideMenus ? 'hidden' : ''}`}
+          onClick={() => router.push('/login')}
+        >Login</Button>
+        }
       </div>
-    </nav>
+      <Popover
+        onOpenChange={(open) => setIsOpen(open)}
+      >
+        <PopoverTrigger className=''>
+        <ChevronDown
+          className={`w-6 h-6 ${isOpen ? 'rotate-180' : ''} duration-200 ease-in-out ${hideMenus ? 'hidden' : ''}`}
+        />
+        </PopoverTrigger>
+        <PopoverContent className='bg-white w-fit rounded-xl'>
+        <ul className='space-y-2 '>
+          <div className='space-y-2 lg:hidden'>
+          {(role === 'pelanggan' ? pelangganMenus : pekerjaMenus).map(menu => (
+            <li key={menu.href}>
+            <Link href={menu.href}>{menu.label}</Link>
+            </li>
+          ))}
+          </div>
+          <li>
+          <Link href='/profile'>Profile</Link>
+          </li>
+          <Separator />
+          <li>
+          <a
+            href='/'
+            onClick={logout}
+            className='text-red-500 cursor-pointer duration-300'
+          >
+            Logout
+          </a>
+          </li>
+        </ul>
+        </PopoverContent>
+      </Popover>
+      </div>
+    </motion.nav>
   )
 }
