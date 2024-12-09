2b806c56baf8a5d92f6a1973b544f412be713568
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-12-09 20:22:35 +0700

    feat: add trigger

--- database/sql/TK2_A_ngeQuery_Trigger_4.sql
+++ database/sql/TK2_A_ngeQuery_Trigger_4.sql
@@ -1,5 +1,4 @@
 -- Trigger to check if pesanan selesai, tf mpay ke pekerja
-
 CREATE OR REPLACE FUNCTION plus_saldo_on_selesai() RETURNS trigger AS $$
     DECLARE
     nominal DECIMAL;
@@ -36,39 +35,6 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER trigger_plus_saldo_on_selesai AFTER
 UPDATE ON TR_PEMESANAN_STATUS
 FOR EACH ROW EXECUTE FUNCTION plus_saldo_on_selesai();
-
-
-CREATE OR REPLACE FUNCTION update_pelanggan_level(pelanggan_id UUID) RETURNS VOID AS $$
-DECLARE
-    transaksi_count INT;
-BEGIN
-    -- Count the number of transactions for the given pelanggan
-    SELECT COUNT(*) INTO transaksi_count
-    FROM TR_PEMESANAN_JASA
-    WHERE idPelanggan = pelanggan_id;
-
-    -- Update the LEVEL based on the number of transactions
-    IF transaksi_count BETWEEN 1 AND 9 THEN
-        UPDATE PELANGGAN SET LEVEL = 'Bronze' WHERE ID = pelanggan_id;
-    ELSIF transaksi_count BETWEEN 10 AND 19 THEN
-        UPDATE PELANGGAN SET LEVEL = 'Silver' WHERE ID = pelanggan_id;
-    ELSIF transaksi_count >= 20 THEN
-        UPDATE PELANGGAN SET LEVEL = 'Gold' WHERE ID = pelanggan_id;
-    ELSE
-        UPDATE PELANGGAN SET LEVEL = NULL WHERE ID = pelanggan_id; -- Default for 0 transactions
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-
-CREATE OR REPLACE FUNCTION trigger_update_pelanggan_level() RETURNS TRIGGER AS $$
-BEGIN
-    -- Call the function to update the pelanggan's level
-    PERFORM update_pelanggan_level(NEW.idPelanggan);
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
 
 CREATE TRIGGER after_insert_update_pemesanan AFTER
 INSERT
